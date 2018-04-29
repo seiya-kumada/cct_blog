ac0182e08883c172c9b9a0dcca59e6bd4167c128
seiya_kumada <kumada@kumadakiyoshinarinoMacBook-Pro.local>
2018-04-29 21:11:54 +0900

    ...先は長い。

--- stan/main.py
+++ stan/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # import pystan
 # import numpy as np
+import matplotlib.pyplot as plt
 
 
 def define_model():
@@ -19,7 +20,11 @@ def define_model():
         transformed parameters {
             int<lower=0> M; // the number of dimensions
             matrix[N, M] x_matrix;
-            for (0 in 0:??
+            for (j in 0:N) {
+                for (i in 0:M) {
+                    matrix[i, j] = x[i]^j 
+                }
+            }
         }
         model {
             y ~ normal(alpha + beta * x, sigma);
@@ -27,6 +32,23 @@ def define_model():
     """
 
 
+def dataset_generator(path):
+    for line in open(path):
+        tokens = line.strip().split()
+        if len(tokens) == 2:
+            yield float(tokens[0]), float(tokens[1])
+
+
 if __name__ == '__main__':
-    model = define_model()
-    print(model)
+    # model = define_model()
+    # print(model)
+    xs = []
+    ys = []
+    for (x, y) in dataset_generator('./dataset.txt'):
+        xs.append(x)
+        ys.append(y)
+    print(xs[0], ys[0])
+    print(xs[-1], ys[-1])
+
+    plt.scatter(xs, ys)
+    plt.show()
