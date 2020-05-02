5fa10a1090ccc43c7269e2647bc33531425b7027
seiya_kumada <kumada@kumadakiyoshinarinoMacBook-Pro.local>
2020-05-02 15:01:39 +0900

    ...

--- abc_conjecture/generate_disjoint_numbers.py
+++ abc_conjecture/generate_disjoint_numbers.py
@@ -4,35 +4,45 @@
 
 class Node:
     COUNTER = 0
-    UPPER_BOUND = 100
 
-    def __init__(self, m, n):
-        self.node1_ = None
-        self.node2_ = None
-        self.node3_ = None
-        self.m_ = m
-        self.n_ = n
+    def __init__(self, m, n, values):
+        self.m = m
+        self.n = n
+        self.values = values
         if n != 1:
             assert((m % n != 0) and (n % m != 0))
-        print("{},{}".format(m, n))
+        # print("{} {},{}".format(Node.COUNTER, m, n))
+        values.append((m, n))
         Node.COUNTER += 1
 
-    def generate(self):
-        if Node.COUNTER < Node.UPPER_BOUND:
-            m = self.m_
-            n = self.n_
-            self.node1_ = Node(2 * m - n, m)
-            self.node2_ = Node(2 * m + n, m)
-            self.node3_ = Node(m + 2 * n, n)
-            self.node1_.generate()
-            self.node2_.generate()
-            self.node3_.generate()
+    def generate(self, upper_bound):
+        if Node.COUNTER < upper_bound:
+            m = self.m
+            n = self.n
+            node1 = Node(2 * m - n, m, self.values)
+            node2 = Node(2 * m + n, m, self.values)
+            node3 = Node(m + 2 * n, n, self.values)
+            node1.generate(upper_bound)
+            node2.generate(upper_bound)
+            node3.generate(upper_bound)
+
+
+class Generator:
+
+    def generate(self, upper_bound):
+        values = []
+        node = Node(2, 1, values)
+        node.generate(upper_bound)
+
+        Node.COUNTER = 0
+        node = Node(3, 1, values)
+        node.generate(upper_bound)
+        return values
 
 
 if __name__ == "__main__":
-    node = Node(2, 1)
-    node.generate()
-
-    Node.COUNTER = 0
-    node = Node(3, 1)
-    node.generate()
+    gen = Generator()
+    values = gen.generate(100)
+    print(len(values))
+    for m, n in values:
+        print(m, n)
