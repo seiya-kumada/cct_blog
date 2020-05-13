a9aab78a1e81e7ebb9e43b8da7862c078c6798f9
seiya_kumada <kumada@kumadakiyoshinarinoMacBook-Pro.local>
2020-05-13 22:33:55 +0900

    実装中。

--- pyro/qs_updater.py
+++ pyro/qs_updater.py
@@ -1,12 +1,57 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import categorical as ca
+import utils
+import unittest
+import torch
 
 
 class QsUpdater:
 
-    def __init__(self, pi):
+    def __init__(self, pi, K):
         self.pi = pi
+        self.K = K
 
-    def update(self, dataset):
-        pass
+    def update(self, W, nu, m, beta, alpha, dataset):
+        N, _ = dataset.size()
+        r = torch.empty(N, self.K)
+        for n in range(N):
+            for k in range(self.K):
+                Wk = W[k]
+                nk = nu[k]
+                Lambda = utils.update_with_4_119(Wk, nk)
+                x = dataset[n]
+                a = torch.dot(x, torch.matmul(Lambda, x))
+                mk = m[k]
+                Lambda_mu = utils.update_with_4_121(Wk, nk, mk)
+                b = x.dot(Lambda_mu)
+                bk = beta[k]
+                c = utils.update_with_122(Wk, nk, mk, bk)
+                d = utils.update_with_120(Wk, nk)
+                alpha_k = alpha[k]
+                e = utils.update_with_4_62(alpha_k, k)
+                f = -0.5 * a + b - 0.5 * c + 0.5 * d + e
+                r[n, k] = torch.exp(f)
+
+        factors = torch.sum(r, dim=1)
+        for n in range(N):
+            r[n] = r[n] / factors[n]
+        self.pi = r
+
+
+class TestQsUpdater(unittest.TestCase):
+
+    def test_(self):
+        dataset = torch.tensor([[2.0, 1.0], [3.0, 2.0]])
+        K = 3
+        pi = torch.empty(2, K)
+        updater = QsUpdater(pi, K=K)
+        W = torch.empty(K, 2, 2)
+        nu = torch.empty(K, 2)
+        m = torch.empty(K, 2)
+        beta = torch.emtpy(K)
+        alpha = torch.empty(K)
+        updater.update(W, nu, m, beta, alpha, dataset)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- pyro/utils.py
+++ pyro/utils.py
@@ -2,15 +2,69 @@
 # -*- coding:utf-8 -*-
 import torch
 import math
+import unittest
 
 
-def update_4_119(W, nu):
+def update_with_4_119(W, nu):
     return nu * W
 
 
-def update_4_120(W, nu):
+def update_with_4_120(W, nu):
     dim, _ = W.size()
     a = sum([torch.digamma(torch.tensor([(nu + 1.0 - d) * 0.5])).item() for d in range(dim)])
     b = torch.logdet(W).item()
     c = dim * math.log(2)
     return a + b + c
+
+
+def update_with_4_121(W, nu, m):
+    return nu * torch.matmul(W, m)
+
+
+def update_with_4_122(W, nu, m, beta):
+    dim, _ = W.size()
+    return nu * m.dot(torch.matmul(W, m)) + dim / beta
+
+
+def update_with_4_62(alpha, index_k):
+    a = torch.sum(alpha)
+    return torch.digamma(torch.tensor([alpha[index_k]])) - torch.digamma(torch.tensor([a]))
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_update_with_4_119(self):
+        nu = 5
+        W = torch.eye(2)
+        a = update_with_4_119(W, nu)
+        self.assertTrue(torch.all(a == torch.tensor([[5, 0], [0, 5]])))
+
+    def test_update_with_4_120(self):
+        nu = 5
+        W = torch.eye(2)
+        a = update_with_4_120(W, nu)
+        self.assertAlmostEqual(a, 3.01223533964696)
+
+    def test_update_with_4_121(self):
+        m = torch.tensor([2, 2.0])
+        W = torch.eye(2)
+        nu = 5
+        a = update_with_4_121(W, nu, m)
+        self.assertTrue(torch.all(a == torch.tensor([10.0, 10.0])))
+
+    def test_update_with_4_122(self):
+        m = torch.tensor([2, 2.0])
+        W = torch.eye(2)
+        nu = 5
+        beta = 1
+        a = update_with_4_122(W, nu, m, beta)
+        self.assertTrue(42.0 == a)
+
+    def test_update_with_4_62(self):
+        alpha = torch.tensor([1.0, 1.0])
+        a = update_with_4_62(alpha, 0)
+        self.assertAlmostEqual(-1.0, a, delta=1.0e-5)
+
+
+if __name__ == "__main__":
+    unittest.main()
