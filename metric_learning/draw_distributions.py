8ae0f8d5268e831cb9dceef82ff245914006dd32
seiya_kumada <kumada@kumadakiyoshinarinoMacBook-Pro.local>
2020-09-21 17:10:31 +0900

    ...

---
+++ metric_learning/draw_distributions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- conding:utf-8 -*-
+import pickle
+import sklearn.manifold as manifold
+import matplotlib.pyplot as plt
+
+FEATURE_PATH = "/Users/kumada/Data/cct_blog/metric_learning/features/metric_100_features.pkl"
+
+
+def store(l, fs, rls, rfs):
+    for f in fs:
+        rls.append(l)
+        rfs.append(f)
+
+
+if __name__ == "__main__":
+    data = pickle.load(open(FEATURE_PATH, "rb"))
+    rlabels = []
+    rfeatures = []
+    for (label, features) in data.items():
+        store(label, features, rlabels, rfeatures)
+
+    reduced_data = manifold.TSNE(n_components=2, random_state=0).fit_transform(rfeatures)
+    plt.scatter(reduced_data[:, 0], reduced_data[:, 1], c=rlabels, marker='.')
+    plt.colorbar()
+
+    plt.savefig("./distribution.jpg")
+    # plt.show()
