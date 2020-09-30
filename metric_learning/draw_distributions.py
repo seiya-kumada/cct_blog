7c72ee4e3a0b92b45af0d04d26ccb2ec8d1369ac
Seiya Kumada <seiya.kumada@cct-inc.co.jp>
2020-09-30 18:09:10 +0900

    ネットワーク変更結果を描画した。精度良くならず。

--- metric_learning/draw_distributions.py
+++ metric_learning/draw_distributions.py
@@ -4,7 +4,7 @@ import pickle
 import sklearn.manifold as manifold
 import matplotlib.pyplot as plt
 
-FEATURE_PATH = "/Users/kumada/Data/cct_blog/metric_learning/features/metric_100_features.pkl"
+FEATURE_PATH = "/Users/kumada/Data/cct_blog/metric_learning/features/metric_16_features.pkl"
 
 
 def store(l, fs, rls, rfs):
